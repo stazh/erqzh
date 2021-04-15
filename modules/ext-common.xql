stazh/erqzh

commit fb22a29f1227f4fdf60b74cfd13d5d697af5c45b

fix(odd): Fix href for internal doc links containing "suche/detail"
Interim solution until we get updated TEI documents, where the href
part "suche/detail" will be entirely deleted.

Closes https://gitlab.existsolutions.com/rqzh/rqzh2/issues/26

expath-pkg.xml
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<package xmlns="http://expath.org/ns/pkg" spec="1.0" version="0.1.14" name="http://existsolutions.com/rqzh2" abbrev="rqzh2">
+<package xmlns="http://expath.org/ns/pkg" spec="1.0" version="0.1.15" name="http://existsolutions.com/rqzh2" abbrev="rqzh2">
     <title>Rechtsquellen des Kantons Zürich</title>
     <dependency package="http://exist-db.org/apps/shared"/>
     <dependency package="http://existsolutions.com/apps/tei-publisher-lib" version="2.8.11"/>

modules/ext-common.xql
@@ -92,7 +92,7 @@ declare function pmf:ref-link($target as xs:string?) {
         else if (starts-with($target, '/suche/detail'))
         then (
             let $volume-name := substring-after($target, 'detail')
-            let $new-href := concat('..', $volume-name, '.xml')
+            let $new-href := concat($volume-name, '.xml')
             return
                 $new-href
         )

resources/odd/rqzh.odd
@@ -1118,7 +1118,7 @@
                 <elementSpec ident="ref" mode="change">
                     <model behaviour="link" predicate="parent::bibl" cssClass="ref-link">
                         <param name="text" value="."/>
-                        <param name="link" value="ec:ref-link(@target)"/>
+                        <param name="link" value="'../' || substring-after(util:collection-name(.), '/db/apps/rqzh-data/data/') || ec:ref-link(@target)"/>
                     </model>
                     <model predicate="ancestor::div/@type='collection'" behaviour="link">
                         <desc>Link in Klammerdokument</desc>

commit 4bd86c3e0ab340fa9627f22a717ac834f36b1334

fix(odd): Fix href for internal doc links #26
* fix side effects caused by fb22a29
* handle all possible link combinations in ext-common.xql instead of
the odd

Additional fixes for
https://gitlab.existsolutions.com/rqzh/rqzh2/issues/26

expath-pkg.xml
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<package xmlns="http://expath.org/ns/pkg" spec="1.0" version="0.1.15" name="http://existsolutions.com/rqzh2" abbrev="rqzh2">
+<package xmlns="http://expath.org/ns/pkg" spec="1.0" version="0.1.16" name="http://existsolutions.com/rqzh2" abbrev="rqzh2">
     <title>Rechtsquellen des Kantons Zürich</title>
     <dependency package="http://exist-db.org/apps/shared"/>
     <dependency package="http://existsolutions.com/apps/tei-publisher-lib" version="2.8.11"/>

modules/ext-common.xql
@@ -92,10 +92,15 @@ declare function pmf:ref-link($target as xs:string?) {
         else if (starts-with($target, '/suche/detail'))
         then (
             let $volume-name := substring-after($target, 'detail')
-            let $new-href := concat($volume-name, '.xml')
+            (: let $new-href := concat('../', $volume-name, '.xml') :)
+            let $collection-name := substring-after(util:collection-name(.),'/db/apps/rqzh-data/data/')
+            let $new-href := concat('../', $collection-name, $volume-name, '.xml')
             return
                 $new-href
         )
+        else if (starts-with($target, 'http'))
+        then
+            $target
         else
             $target
 
@@ -203,7 +208,7 @@ declare function pmf:get-canton($id as xs:string?) {
     let $components := tokenize($id, "_")
     return
         $components[2]
-}; 
+};
 
 declare function pmf:format-id($id as xs:string?) {
     let $temp  := replace($id, "^(.+?)_(\d{3}.*?)(?:_\d{1,2})?$", "$1 $2")

resources/odd/rqzh.odd
@@ -1118,7 +1118,7 @@
                 <elementSpec ident="ref" mode="change">
                     <model behaviour="link" predicate="parent::bibl" cssClass="ref-link">
                         <param name="text" value="."/>
-                        <param name="link" value="'../' || substring-after(util:collection-name(.), '/db/apps/rqzh-data/data/') || ec:ref-link(@target)"/>
+                        <param name="link" value="ec:ref-link(@target)"/>
                     </model>
                     <model predicate="ancestor::div/@type='collection'" behaviour="link">
                         <desc>Link in Klammerdokument</desc>