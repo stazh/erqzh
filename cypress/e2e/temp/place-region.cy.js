130ce0c79d78f41f3589cade2b6806c60ca6da97
Iwona Grabska-Gradzińska <grabska@gmail.com>
2023-07-28 13:44:36 +0200

    search field test

--- cypress/e2e/temp/place-region.cy.js
+++ cypress/e2e/temp/place-region.cy.js
@@ -7,6 +7,41 @@ describe('place-list page for ZH_NF_I_1_11', () => {
         cy.get('.pb-container #map').should('be.visible')
     })
 
+    it('display headline', () => {
+        cy.contains('h1', 'Places')
+    })
+
+
+    it('display Albis', () => {
+        cy.contains('a', 'Albis')
+        .should('have.attr', 'href', 'Affoltern am Albis?&category=A&search=&key=loc011161')
+    })
+
+    it('test each', () => {
+        cy.get('pb-split-list .place').each(() => { //$el, index, $list
+            // console.log($el, index, $list)
+        }) 
+    
+    })
+
+    it('test each A place', () => {    
+        cy.get('.place a')
+        .should('have.length', 16)
+        .each(($place, index, $lis) => {
+            const text = $place.text() + "?&category=A"
+            const href = cy.wrap($place).invoke('attr', 'href')
+            cy.wrap($place).invoke('attr', 'href').should('include', text)
+            console.log('AAAAAAAAAAAAA')
+            console.log(text, href)
+            
+        })
+        // .then(($lis) => {
+        // expect($lis).to.have.length(16) // true
+        // })
+
+    })
+
+
     // it('the city is Basel', () => {
     //     cy.get('#locations pb-geolocation')
     //     .should('be.visible')
@@ -21,8 +56,12 @@ describe('place-list page for ZH_NF_I_1_11', () => {
     //does not work
     it('Search for “an”', () => {
         // cy.get('#input-1 > input')
-        cy.get('#input-1 > input').log()
+        console.clear()
+        console.log('Aaaaaa')
+        console.log(cy.get('[name="search"]').first())
+        cy.get('[name="search"]').first().focus()
         .type('an')
+        cy.get('#query > [role=button]').click()
         
         cy.get('.place')
         .should('have.length', 3)
